b33295455464671756c2d564b7fd8dc625a3be2e
ilopezdeaudikana <ilopezdeaudikana@gmail.com>
2026-02-26 19:06:07 +0000

    opposite

--- src/services/challenge.service.ts
+++ src/services/challenge.service.ts
@@ -5,7 +5,7 @@ import * as responseSample from '../sample-response.json'
 const MASTRA_API_PROTOCOL = import.meta.env.VITE_MASTRA_API_PROTOCOL
 const MASTRA_API_HOST = import.meta.env.VITE_MASTRA_API_HOST 
 const MASTRA_API_PORT = import.meta.env.VITE_MASTRA_API_PORT 
-const MASTRA_API_BASE_URL = `${MASTRA_API_PROTOCOL}://${MASTRA_API_HOST}:${MASTRA_API_PORT}`
+const MASTRA_API_BASE_URL = process.env.NODE_ENV === 'development'? `${MASTRA_API_PROTOCOL}://${MASTRA_API_HOST}:${MASTRA_API_PORT}`: '/'
 
 const getApiUrl = (path: string) => new URL(`api/${path}`, `${MASTRA_API_BASE_URL}`).toString()
 
